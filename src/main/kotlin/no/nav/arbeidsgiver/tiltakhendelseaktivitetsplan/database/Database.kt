3bb783a089ca8566afd4dffd9341d572a26c8a8f
Mattias Lundmark <lundmark.mattias@gmail.com>
2022-10-24 15:41:23 +0200

    Litt database-wiring

---
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/database/Database.kt
@@ -0,0 +1,42 @@
+package no.nav.arbeidsgiver.tiltakhendelseaktivitetsplan.database
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import kotliquery.queryOf
+import kotliquery.sessionOf
+import kotliquery.using
+import org.flywaydb.core.Flyway
+
+class Database {
+    private val dataSource: HikariDataSource = hikari()
+
+    init {
+        val flyway = Flyway.configure()
+            .locations("db.migration")
+            .dataSource(dataSource)
+            .load()
+        flyway.migrate()
+    }
+
+    companion object {
+        private fun hikari(): HikariDataSource {
+            val config = HikariConfig()
+            config.driverClassName = "org.h2.Driver"
+            config.jdbcUrl = "jdbc:h2:mem:test"
+            config.maximumPoolSize = 3
+            config.isAutoCommit = false
+            config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+            config.validate()
+            return HikariDataSource(config)
+        }
+    }
+
+
+    fun runQuery() {
+        using(sessionOf(dataSource)) { session ->
+            session.run(queryOf("""
+                select 1 from avtale_hendelse
+            """.trimIndent()).asExecute)
+        }
+    }
+}
