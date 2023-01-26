1451afed666251bca539c25d4391f85b741881fb
Mattias Lundmark <lundmark.mattias@gmail.com>
2023-01-26 12:26:16 +0100

    Leser fra compacted topic

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/Topics.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/Topics.kt
@@ -1,5 +1,5 @@
 package no.nav.arbeidsgiver.tiltakhendelseaktivitetsplan.kafka
 
 object Topics {
-    const val AVTALE_HENDELSE = "arbeidsgiver.tiltak-avtale-hendelse"
+    const val AVTALE_HENDELSE = "arbeidsgiver.tiltak-avtale-hendelse-compact"
 }
