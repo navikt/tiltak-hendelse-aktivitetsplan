49a6c15a472d4c0be07ad54f2c576d2de2e7ed70
Mattias Lundmark <lundmark.mattias@gmail.com>
2023-02-06 15:18:36 +0100

    Ny consumer group

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/consumerConfig.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/consumerConfig.kt
@@ -8,7 +8,7 @@ import java.util.*
 
 fun consumerConfig() = Properties().apply {
     put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1)
-    put(ConsumerConfig.GROUP_ID_CONFIG, "tiltak-hendelse-aktivitetsplan-1")
+    put(ConsumerConfig.GROUP_ID_CONFIG, "tiltak-hendelse-aktivitetsplan-2")
     put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
     put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
     put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
