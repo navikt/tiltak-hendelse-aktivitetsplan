3036951d3a469e30728e3d31e9ef5ec72165792b
d142796 <millad.dagdoni@nav.no>
2024-04-02 17:22:43 +0200

    satt tilbake consumer poll duration til 5 sekunder (originalt verdi) i FeilConsumer

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/FeilConsumer.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/FeilConsumer.kt
@@ -23,7 +23,7 @@ class FeilConsumer(
             .registerModule(JavaTimeModule())
         consumer.subscribe(listOf(Topics.AKTIVITETSPLAN_FEIL))
         while (true) {
-            val records: ConsumerRecords<String, String> = consumer.poll(Duration.ofSeconds(20))
+            val records: ConsumerRecords<String, String> = consumer.poll(Duration.ofSeconds(5))
             records.isEmpty && continue
             records.forEach {
                 /*val melding: AktivitetsPlanFeilMelding = mapper.readValue(it.value())
