1c7e7b99338501dcd4ff1ad397ef183c9ed3d64c
d142796 <millad.dagdoni@nav.no>
2024-04-10 15:36:23 +0200

    Revertet tilbake hvordan FeilConsumer var opprinnelig uten kommentarer og println()

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/FeilConsumer.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/FeilConsumer.kt
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import no.nav.arbeidsgiver.tiltakhendelseaktivitetsplan.database.AktivitetsplanMeldingEntitet
 import no.nav.arbeidsgiver.tiltakhendelseaktivitetsplan.database.Database
 import no.nav.arbeidsgiver.tiltakhendelseaktivitetsplan.utils.log
 import org.apache.kafka.clients.consumer.Consumer
@@ -19,32 +16,23 @@ class FeilConsumer(
     private val consumer: Consumer<String, String>,
     private val database: Database
 ) {
-    val mapper = jacksonObjectMapper()
-        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        .registerModule(JavaTimeModule())
-
-    fun start() = runBlocking  {
+    fun start() = runBlocking {
         log.info("Starter konsumering på topic: ${Topics.AKTIVITETSPLAN_FEIL}")
-
+        val mapper = jacksonObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .registerModule(JavaTimeModule())
         consumer.subscribe(listOf(Topics.AKTIVITETSPLAN_FEIL))
         while (true) {
             val records: ConsumerRecords<String, String> = consumer.poll(Duration.ofSeconds(5))
-            println("FeilConsumer: records empty? : ${records.isEmpty}")
             records.isEmpty && continue
             records.forEach {
                 val melding: AktivitetsPlanFeilMelding = mapper.readValue(it.value())
-
                 val avtaleId = it.key(); // Kafka key er funksjonell id som altså skal være avtaleId.
-                val hendelseMelding: List<AktivitetsplanMeldingEntitet>? =  database.hentEntitetMedAvtaleId(UUID.fromString(avtaleId));
-                println("FeilConsumer MELDING ID ${avtaleId} type: ${melding.errorType} og finnes den i databasen?  ${hendelseMelding?.size}") //TODO Fjern meg
-
+                val hendelseMelding = database.hentEntitetMedAvtaleId(UUID.fromString(avtaleId));
+                // Log error om det er en melding vi har sendt
                 if (!hendelseMelding.isNullOrEmpty()) {
-                   println("EN FEIL MELDING SOM SKAL IGNORERES") //TODO Fjern meg
-                   log.error("Feil fra aktivitetsplan for avtale ${avtaleId}. Feilmelding: ${melding.errorMessage}");
-               }
-
-                //TODO: SJekk om feilmelding er til oss og send en ny melding tilbake til aktivitetsplan
-
+                    log.error("Feil fra aktivitetsplan for avtale ${avtaleId}. Feilmelding: ${melding.errorMessage}");
+                }
             }
             consumer.commitAsync()
         }
