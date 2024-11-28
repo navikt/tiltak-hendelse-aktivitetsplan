08a5222809e096ba5ba79be37571604a55ea2c25
Eirik Vigeland <eirik.vigeland@nav.no>
2024-11-28 14:17:06 +0100

    feat. starte behandling av Arbeidstrening
    MÅ IKKE MERGES FØR VI TAR OVER ARBTREN FRA ARENA

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/Tiltakstype.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/Tiltakstype.kt
@@ -1,7 +1,7 @@
 package no.nav.arbeidsgiver.tiltakhendelseaktivitetsplan.kafka
 
 enum class Tiltakstype(val beskrivelse: String, val skalTilAktivitetsplan: Boolean) {
-    ARBEIDSTRENING("Arbeidstrening", false),
+    ARBEIDSTRENING("Arbeidstrening", true),
     MIDLERTIDIG_LONNSTILSKUDD("Midlertidig lønnstilskudd", true),
     VARIG_LONNSTILSKUDD("Varig lønnstilskudd", true),
     MENTOR("Mentor", false),
