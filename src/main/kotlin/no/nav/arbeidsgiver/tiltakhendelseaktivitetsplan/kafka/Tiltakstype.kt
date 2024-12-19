e5b7f165969cabd29ad4d8eff2002fed3a34488a
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-12-19 16:02:18 +0100

    VTAO i liste over tiltakstyper i enum

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/Tiltakstype.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/Tiltakstype.kt
@@ -6,5 +6,7 @@ enum class Tiltakstype(val beskrivelse: String, val skalTilAktivitetsplan: Boole
     VARIG_LONNSTILSKUDD("Varig lønnstilskudd", true),
     MENTOR("Mentor", false),
     INKLUDERINGSTILSKUDD("Inkluderingstilskudd", false),
-    SOMMERJOBB("Sommerjobb", false);
+    SOMMERJOBB("Sommerjobb", false),
+    // TODO: skal til aktivitetsplan
+    VTAO("Varig tilrettelagt arbeid i ordinær virksomhet", false);
 }
