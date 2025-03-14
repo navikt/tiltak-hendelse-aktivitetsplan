4f660dae3fb3acf634b91ad3be811b33a30ea487
Eirik Vigeland <eirik.vigeland@nav.no>
2025-03-14 14:13:40 +0100

    fix. Aktivitetsplanen sender timestamps med zone for AktivitetsPlanFeilMelding

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/AktivitetsPlanFeilMelding.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/AktivitetsPlanFeilMelding.kt
@@ -1,9 +1,9 @@
 package no.nav.arbeidsgiver.tiltakhendelseaktivitetsplan.kafka
 
-import java.time.LocalDateTime
+import java.time.ZonedDateTime
 
 data class AktivitetsPlanFeilMelding(
-    val timestamp: LocalDateTime,
+    val timestamp: ZonedDateTime,
     val failingMessage: String,
     val errorMessage: String
 )
