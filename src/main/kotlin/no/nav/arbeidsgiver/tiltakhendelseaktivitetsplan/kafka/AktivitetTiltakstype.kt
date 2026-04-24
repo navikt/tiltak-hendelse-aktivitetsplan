fff0162a0687757c85f969fedb65db862435f0ab
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2026-04-24 13:18:46 +0200

    Fireårig lønnstilskudd skal til aktivitetsplanen

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/AktivitetTiltakstype.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/AktivitetTiltakstype.kt
@@ -1,23 +1,25 @@
 package no.nav.arbeidsgiver.tiltakhendelseaktivitetsplan.kafka
 
 enum class AktivitetTiltakstype {
-    MIDLERTIDIG_LONNSTILSKUDD,
-    VARIG_LONNSTILSKUDD,
     ARBEIDSTRENING,
-    VARIG_TILRETTELAGT_ARBEID_I_ORDINAER_VIRKSOMHET,
+    FIREARIG_LONNSTILSKUDD,
+    MENTOR,
+    MIDLERTIDIG_LONNSTILSKUDD,
     SOMMERJOBB,
-    MENTOR;
+    VARIG_LONNSTILSKUDD,
+    VARIG_TILRETTELAGT_ARBEID_I_ORDINAER_VIRKSOMHET;
 
     companion object {
         fun parse(tiltakstype: Tiltakstype): AktivitetTiltakstype {
             return when (tiltakstype) {
-                Tiltakstype.MIDLERTIDIG_LONNSTILSKUDD -> MIDLERTIDIG_LONNSTILSKUDD
-                Tiltakstype.VARIG_LONNSTILSKUDD -> VARIG_LONNSTILSKUDD
                 Tiltakstype.ARBEIDSTRENING -> ARBEIDSTRENING
-                Tiltakstype.VTAO -> VARIG_TILRETTELAGT_ARBEID_I_ORDINAER_VIRKSOMHET
-                Tiltakstype.SOMMERJOBB -> SOMMERJOBB
+                Tiltakstype.FIREARIG_LONNSTILSKUDD -> FIREARIG_LONNSTILSKUDD
                 Tiltakstype.MENTOR -> MENTOR
-                else -> throw IllegalArgumentException("Ukjent tiltakstype: $tiltakstype")
+                Tiltakstype.MIDLERTIDIG_LONNSTILSKUDD -> MIDLERTIDIG_LONNSTILSKUDD
+                Tiltakstype.SOMMERJOBB -> SOMMERJOBB
+                Tiltakstype.VARIG_LONNSTILSKUDD -> VARIG_LONNSTILSKUDD
+                Tiltakstype.VTAO -> VARIG_TILRETTELAGT_ARBEID_I_ORDINAER_VIRKSOMHET
+                Tiltakstype.INKLUDERINGSTILSKUDD -> throw IllegalArgumentException("Tiltakstype har ingen tilhørende aktivitetskort-type: $tiltakstype")
             }
         }
 
--- src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/Tiltakstype.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakhendelseaktivitetsplan/kafka/Tiltakstype.kt
@@ -2,10 +2,11 @@ package no.nav.arbeidsgiver.tiltakhendelseaktivitetsplan.kafka
 
 enum class Tiltakstype(val beskrivelse: String, val skalTilAktivitetsplan: Boolean) {
     ARBEIDSTRENING("Arbeidstrening", true),
-    MIDLERTIDIG_LONNSTILSKUDD("Midlertidig lønnstilskudd", true),
-    VARIG_LONNSTILSKUDD("Varig lønnstilskudd", true),
-    MENTOR("Mentor", true),
+    FIREARIG_LONNSTILSKUDD("Fireårig lønnstilskudd for unge", true),
     INKLUDERINGSTILSKUDD("Inkluderingstilskudd", false),
+    MENTOR("Mentor", true),
+    MIDLERTIDIG_LONNSTILSKUDD("Midlertidig lønnstilskudd", true),
     SOMMERJOBB("Sommerjobb", true),
-    VTAO("Varig tilrettelagt arbeid i ordinær virksomhet", true);
+    VARIG_LONNSTILSKUDD("Varig lønnstilskudd", true),
+    VTAO("Varig tilrettelagt arbeid i ordinær virksomhet", true)
 }
